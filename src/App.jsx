8ca08b8c4040511be398c250b7e23af6679151c7
S B <scottb4545@gmail.com>
2023-01-23 18:36:11 +0000

    basic structure set

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,19 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
+import cardBack from './assets/bg-card-back.png';
+import cardFront from './assets/bg-card-front.png';
 import './App.scss';
-
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="bg-image">
+      <div className="credit-card-section">
+        <div className="card-front">
+          <img src={cardFront} alt="front-of-card-image" />
+        </div>
+        <div className="card-back">
+          <img src={cardBack} alt="back-of-card-image" />
+        </div>{' '}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="crrdit-card-form">FORM HERE!</div>
     </div>
   );
 }
