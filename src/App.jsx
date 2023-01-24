9d4f7d281acac52fa289f2594c466b89a65efac3
S B <scottb4545@gmail.com>
2023-01-24 16:42:04 +0000

    changed to tailwind.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,35 @@
 import { useState } from 'react';
-import cardBack from './assets/bg-card-back.png';
-import cardFront from './assets/bg-card-front.png';
+import bgMobile from './assets/bg-main-mobile.png';
+import bgDesktop from './assets/bg-main-desktop.png';
 import './App.scss';
+import cardLogo from './assets/card-logo.svg';
+
 function App() {
   return (
-    <div className="bg-image">
-      <div className="credit-card-section">
-        <div className="card-front">
-          <img src={cardFront} alt="front-of-card-image" />
-        </div>
-        <div className="card-back">
-          <img src={cardBack} alt="back-of-card-image" />
-        </div>{' '}
+    <section>
+      <div className="absolute -z-10">
+        <picture>
+          <source media="(min-width: 768px)" srcSet={bgDesktop} />
+          <img src={bgMobile} alt="background-image" />
+        </picture>
       </div>
-      <div className="crrdit-card-form">FORM HERE!</div>
-    </div>
+      <div className="grid grid-cols-1 gap-8 lg:grid-cols-2">
+        <div className="mt-10 mx-5 ">
+          <article className="card-front p-5">
+            <img src={cardLogo} alt="card-logo" />
+            <div>
+              <h2>0000 0000 0000 0000</h2>
+              <ul>
+                <li> Jane Appleseed</li>
+                <li> 00/00</li>
+              </ul>{' '}
+            </div>
+          </article>
+          <article className="card-back"></article>
+        </div>
+        <div className="credit-card-form">FORM HERE!</div>
+      </div>
+    </section>
   );
 }
 
