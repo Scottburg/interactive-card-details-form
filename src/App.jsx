deced65f94b6684ce8cd32a500212a0fb2f9a1f4
S B <scottb4545@gmail.com>
2023-01-25 17:09:34 +0000

    working on mobile view

--- src/App.jsx
+++ src/App.jsx
@@ -3,30 +3,54 @@ import bgMobile from './assets/bg-main-mobile.png';
 import bgDesktop from './assets/bg-main-desktop.png';
 import tick from './assets/icon-complete.svg';
 import cardLogo from './assets/card-logo.svg';
+import { format } from 'date-fns';
 
 function App() {
   const emptyCardDetails = {
-    name: null,
-    number: null,
-    expiry: null,
-    cvc: null,
+    name: '',
+    number: '',
+    expiry: '01/01',
+    cvc: '',
   };
   const [success, setSuccess] = useState(false);
   const [cardDetails, setCardDetails] = useState(emptyCardDetails);
+
+  // const convertToCardNumber = (num) => {
+  //   // let num = '1234567812345678';
+  //   let formattedNumber = '';
+  //   for (let i = 0; i < num.length; i += 4) {
+  //     formattedNumber += num.substring(i, i + 4) + ' ';
+  //   }
+  //   console.log(
+  //     '🚀 ~ file: App.jsx:25 ~ convertToCardNumber ~ formattedNumber.trim()',
+  //     formattedNumber.trim()
+  //   );
+  //   return formattedNumber.trim();
+  // };
+
+  const handleCardNumber = (e) => {
+    if (e.target.value.length <= 19) {
+      setCardDetails({
+        ...cardDetails,
+        number: e.target.value,
+      });
+    }
+  };
+
   return (
     <section>
-      <div className="absolute -z-10">
+      <div className="absolute -z-10 w-full h-full">
         <picture>
           <source media="(min-width: 768px)" srcSet={bgDesktop} />
           <img
-            className="min-w-screen md:min-h-screen"
+            className="w-full md:w-1/3  md:h-full object-cover"
             src={bgMobile}
             alt="background-image"
           />
         </picture>
       </div>
       <div className="grid grid-cols-1 gap-8 md:grid-cols-2 max-w-7xl mx-auto">
-        <div className="mt-10 mx-5 md:grid md:grid-cols-1 md:gap-8 ">
+        <div className="mt-10 mx-5 grid grid-cols-1 gap-4 md:gap-0 ">
           <article className="card-front p-5 flex flex-col justify-between">
             <img className="w-14 md:w-20" src={cardLogo} alt="card-logo" />
             <div>
@@ -40,7 +64,9 @@ function App() {
                   {cardDetails.name ? cardDetails.name : 'Jane Appleseed'}
                 </li>
                 <li className="text-white  text-xs md:text-sm tracking-wider">
-                  {cardDetails.expiry ? cardDetails.expiry : '00/00'}
+                  {cardDetails.expiry
+                    ? format(new Date(cardDetails.expiry), 'MM yy')
+                    : '00/00'}
                 </li>
               </ul>{' '}
             </div>
@@ -51,11 +77,11 @@ function App() {
             </p>
           </article>
         </div>
-        <div className="credit-card-form">
+        <div className="credit-card-form pt-8 px-5 pb-20">
           {success ? (
             <SuccessScreen setSuccess={setSuccess} />
           ) : (
-            <form className="flex flex-col justify-center  gap-8 max-w-lg h-screen">
+            <form className="flex flex-col justify-center  gap-8 max-w-lg md:h-screen">
               <div>
                 <label htmlFor="cardholder_name">Cardholder Name</label>
                 <input
@@ -71,6 +97,7 @@ function App() {
                 />
               </div>
               <div>
+                {/* //Add formating for spaces in card number */}
                 <label htmlFor="card_number">Card Number</label>
                 <input
                   type="number"
@@ -78,11 +105,9 @@ function App() {
                   id="card_number"
                   placeholder="e.g. 1234 5678 9123 0000"
                   required
-                  maxLength={16}
+                  maxLength="19"
                   value={cardDetails.number}
-                  onChange={(e) => {
-                    setCardDetails({ ...cardDetails, number: e.target.value });
-                  }}
+                  onChange={handleCardNumber}
                 />
               </div>
               <article className="flex justify-between items-center gap-8">
@@ -115,7 +140,9 @@ function App() {
                     required
                     value={cardDetails.cvc}
                     onChange={(e) => {
-                      setCardDetails({ ...cardDetails, cvc: e.target.value });
+                      if (e.target.value.length <= 3) {
+                        setCardDetails({ ...cardDetails, cvc: e.target.value });
+                      }
                     }}
                   />
                 </div>
@@ -139,7 +166,7 @@ function App() {
 const SuccessScreen = ({ setSuccess }) => {
   return (
     <>
-      <div className="flex flex-col items-center justify-center h-screen max-w-lg mx-auto ">
+      <div className="flex flex-col items-center justify-center md:h-screen max-w-lg mx-auto ">
         {' '}
         <img src={tick} className="block mx-auto" />
         <h1 className="dark-violet text-2xl uppercase text-center my-6 tracking-widest">
